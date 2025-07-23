087838daa7730ecca36cf3e1dfcc9c4304eea4d1
Md. Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-23 16:03:09 +0600

    can create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,36 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  private
+  def post_params
+    params.expect(post: [ :title, :body ])
   end
 
 end
