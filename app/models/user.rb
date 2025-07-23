daa54e28ab47f50f0949b5f038e869cbaec8cefa
Md. Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-23 12:43:45 +0600

    association of user and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :posts, dependent: :destroy
   has_one_attached :profile_photo
-  
+
   validates :username, presence: true, uniqueness: {case_sensitive: false}
   validates :full_name, presence: true
 end
