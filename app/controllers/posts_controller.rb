e0b425112bbef0a720b7cde8f26c475441dab7f0
Md. Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-23 16:19:28 +0600

    can edit and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,26 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    if @post.user_id != current_user.id
+      redirect_to post_path(@post)
+    end
+  end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to user_path
   end
 
   private
