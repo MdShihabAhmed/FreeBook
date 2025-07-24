e111c897091fd2108df7f2d8b58f0df5fad83b4d
Md. Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-24 16:38:23 +0600

    view of comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,17 @@
 class CommentsController < ApplicationController
   def create
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    if @comment
+      redirect_to post_path(@post)
+    else
+      redirect_to post_path(@post), alert: "Comment not created. Please try again."
+    end
   end
 
-
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :user_id)
+  end
 end
