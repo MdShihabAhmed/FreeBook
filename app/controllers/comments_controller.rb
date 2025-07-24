5d56592bb35a628d34c01ffd42a94826f97591c1
Md. Shihab Ahmed <shihab.ahmed@technonext.com>
2025-07-24 17:24:00 +0600

    can comment on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
-    @user = current_user
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    if @comment
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
       redirect_to post_path(@post)
     else
       redirect_to post_path(@post), alert: "Comment not created. Please try again."
@@ -12,6 +13,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:body, :user_id)
+    params.require(:comment).permit(:body)
   end
 end
