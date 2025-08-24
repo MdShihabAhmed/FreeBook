69a398f571d3b8dbbd1c60f07f87df1e0bb0d569
Md. Shihab Ahmed <shihab088068@gmail.com>
2025-08-24 12:12:13 +0600

    test fix

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get comments_create_url
-    assert_response :success
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @post = posts(:one)
+    sign_in @user
+  end
+
+  test "should create comment" do
+    assert_difference("Comment.count") do
+      post post_comments_url(@post), params: { comment: { body: "This is a comment." } }
+    end
+
+    assert_redirected_to post_url(@post)
   end
 end
